88cd2950ae990646b9f51a7269819371b02fe985
garcia32092 <pops32092@gmail.com>
2024-10-16 18:46:33 -0700

    add activity-chart component

---
+++ client/src/app/activity-chart/activity-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-chart.component.html',
+  styleUrl: './activity-chart.component.css'
+})
+export class ActivityChartComponent {
+
+}
