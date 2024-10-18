a71e99d4aacf6a86c665dc7b9649f12e543cdb51
garcia32092 <pops32092@gmail.com>
2024-10-18 01:20:52 -0700

    add activity-data.ts

---
+++ client/src/app/log-activity/activity-data.ts
@@ -0,0 +1,9 @@
+export const activities = ['Running', 'Cycling', 'Swimming', 'Yoga'];
+export const categories = ['Health & Fitness', 'Productivity', 'Leisure', 'Education & Study', 'Social', 'Travel', 'Entertainment', 'Creativity', 'Shopping', 'Cooking & Eating', 'Sleep'];
+export const activityToCategory = {
+  'Running': 'Health & Fitness',
+  'Cycling': 'Health & Fitness',
+  'Swimming': 'Health & Fitness',
+  'Yoga': 'Health & Fitness',
+  // Add more mappings here
+};
