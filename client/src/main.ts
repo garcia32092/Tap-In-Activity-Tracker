2198de19c7087ddb8ab18925c2eaa6097d165fc9
garcia32092 <pops32092@gmail.com>
2024-10-21 21:37:54 -0700

    update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // Use provideHttpClient instead of HttpClientModule
 import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()] // Add HttpClient support globally
+  providers: [
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideRouter(routes)
+  ] // Add HttpClient support globally
 }).catch((err) => console.error(err));
