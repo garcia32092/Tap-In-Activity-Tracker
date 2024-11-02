2632c8ed91721f6163e382f654f3f935fbe630d9
garcia32092 <pops32092@gmail.com>
2024-11-01 19:52:06 -0700

    update imports

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -16,6 +17,7 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatButtonModule,
     CommonModule,
     RouterModule
   ],
