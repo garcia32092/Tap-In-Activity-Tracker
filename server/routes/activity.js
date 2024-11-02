c690258803903ba32bdb2261c45e5a788f64e5b9
garcia32092 <pops32092@gmail.com>
2024-11-01 19:53:07 -0700

    add routes to update & delete by ID

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { logActivity, getActivities } = require('../controllers/activityController');
+const { logActivity, getActivities, deleteActivity, updateActivity } = require('../controllers/activityController');
 const router = express.Router();
 
 // Route to log an activity
@@ -7,5 +7,11 @@ router.post('/', logActivity);
 
 // Route to fetch all activities
 router.get('/', getActivities);
+
+// Route to delete an activity by ID
+router.delete('/:id', deleteActivity);
+
+// Route to update an activity by ID
+router.put('/:id', updateActivity);
 
 module.exports = router;
