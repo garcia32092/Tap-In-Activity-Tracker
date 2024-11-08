3934dd5273a9adf7c57e32c20de5e9b534747a7c
garcia32092 <pops32092@gmail.com>
2024-11-07 20:55:05 -0700

    update app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { ActivityCalendarComponent } from './activity-calendar/activity-calendar.component';
 import { LogActivityComponent } from './log-activity/log-activity.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { LogSelectionComponent } from './log-selection/log-selection.component';
+import { LogJournalComponent } from './log-journal/log-journal.component';
+import { LogSmallWinsComponent } from './log-smallwins/log-smallwins.component';
 
 export const routes: Routes = [
-    { path: '', component: ActivityCalendarComponent },
-  { path: 'log', component: LogActivityComponent },
+  { path: '', component: LogSelectionComponent },
+  { path: 'calendar', component: ActivityCalendarComponent },
   { path: 'list', component: ActivityListComponent },
+  { path: 'log-activity', component: LogActivityComponent },
+  { path: 'log-journal', component: LogJournalComponent },
+  { path: 'log-small-wins', component: LogSmallWinsComponent },
+  { path: '**', redirectTo: '' },
 ];
